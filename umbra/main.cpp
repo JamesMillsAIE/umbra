60ed889f6b453d335acee040c81cec842ed180e5
James Mills <james.mills@aie.edu.au>
2025-05-09 15:44:22 +1000

    umbra window opens

--- umbra/main.cpp
+++ umbra/main.cpp
@@ -1,6 +1,24 @@
+#include <Umbra/Application.h>
+
 #include <iostream>
 
+#if _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+// main that returns an integer
 int main()
 {
-    return EXIT_SUCCESS;
+#if _DEBUG
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    Application* app = new Application();
+
+    int returnState = app->Run();
+
+    delete app;
+
+    return returnState;
 }
